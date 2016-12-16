217d83e716ffd4038fdd325c763dc204cac9d24a
veronicalynn18 <bveronicalynn@gmail.com>
2016-12-15 22:26:22 -0500

    more style

--- js/controller.js
+++ js/controller.js
@@ -8,17 +8,19 @@
           controller: "myController"
         });
 
-
-      });
+    });
 
   // careate a variable for petfinder json
   app.controller("myController", function($scope,$http){
 
-
   var petfinderData;
 
-
   var url = 'https://api.petfinder.com/shelter.getPets?key=d7ca0694f95be8be22ee522ba0c3329b&id=MI995&output=full&format=json'
+
+  $.ajaxSetup({
+    scriptCharset: "utf-8",
+    contentType: "application/json; charset=utf-8"
+  });
 
   $.ajax({
     type: 'GET',
